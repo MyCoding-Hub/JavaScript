1b587a40886b03417a84d5334834dffbc0148a60
S Boopalan <breacher.darkworld@gmail.com>
2023-03-26 08:20:43 +0530

    Create variable.js

---
+++ variable.js
@@ -0,0 +1,15 @@
+console.log("thunder");
+
+let userName= "Thunder Boy";
+
+console.log(userName);
+
+let value1= 5;
+
+let value2=3;
+
+let unit= value1 + value2;
+
+console.log("allezo " + 21);
+
+console.log(unit);
