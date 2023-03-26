1009ae59ffb7e70f5d1e5266e1d1d8767bad098c
S Boopalan <breacher.darkworld@gmail.com>
2023-03-26 08:16:58 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+class bunny{
+
+
+    constructor(name){
+        
+        this.name = name;
+    }
+
+    fun1(x) {
+
+        let na =x ;         
+      console.log("funny bear" + na);
+    }
+
+    fun2(){
+
+        let m = 'hulk'
+        console.log('hello monkey' + m);
+    }
+}
+
+let value = new bunny('thunder boy');
+
+value.fun1('iron man');
+
+value.fun2();
