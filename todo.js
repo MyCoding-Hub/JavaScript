9b823898ad99ae50a7f0e2ca760d0c885b4ebfa8
S Boopalan <breacher.darkworld@gmail.com>
2023-03-27 10:55:14 +0530

    Create todo.js

---
+++ todo.js
@@ -0,0 +1,19 @@
+let todo = ['wakeup','work Out','Video Upload','Practice'];
+
+// let time = [5,6,7,8];
+
+// for(i=0; i < todo.length; i++) {
+
+//     console.log(`${time[i]} You have to ${todo[i]}`)
+// }
+
+todo.push('Eat Healthy'); //to add new item in array
+
+console.log(todo);
+
+todo.unshift('relax'); //to add first in array
+
+console.log(todo);
+
+
+
