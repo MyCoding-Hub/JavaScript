354ba964e1de08bce69195b79214651d392932e8
S Boopalan <breacher.darkworld@gmail.com>
2023-03-28 14:44:15 +0530

    Create == vs ===.js

---
+++ == vs ===.js
@@ -0,0 +1,9 @@
+// == and === both are same but === have some extra features
+
+let hello = {} //empty object declaration
+
+let hello2 = {}
+
+console.log(hello == hello2);
+
+console.log(hello === hello2);
